e9e29239f014e003dc5ead90689ad1b59da8383d
jack adamczyk <adamczyk_jack@ad1.columbusstate.edu>
2023-02-14 16:48:46 -0500

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,163 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Btn1 = New System.Windows.Forms.Button()
+        Me.Btn2 = New System.Windows.Forms.Button()
+        Me.Btn3 = New System.Windows.Forms.Button()
+        Me.Btn4 = New System.Windows.Forms.Button()
+        Me.Btn5 = New System.Windows.Forms.Button()
+        Me.BtnExit = New System.Windows.Forms.Button()
+        Me.LblFrench = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(238, 63)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(306, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Do you know the French words for the numbers"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(339, 92)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(89, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "1 through 5?"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(264, 118)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(234, 17)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Click the buttons below to see them."
+        '
+        'Btn1
+        '
+        Me.Btn1.Location = New System.Drawing.Point(107, 191)
+        Me.Btn1.Name = "Btn1"
+        Me.Btn1.Size = New System.Drawing.Size(75, 23)
+        Me.Btn1.TabIndex = 3
+        Me.Btn1.Text = "Button1"
+        Me.Btn1.UseVisualStyleBackColor = True
+        '
+        'Btn2
+        '
+        Me.Btn2.Location = New System.Drawing.Point(241, 191)
+        Me.Btn2.Name = "Btn2"
+        Me.Btn2.Size = New System.Drawing.Size(75, 23)
+        Me.Btn2.TabIndex = 4
+        Me.Btn2.Text = "Button2"
+        Me.Btn2.UseVisualStyleBackColor = True
+        '
+        'Btn3
+        '
+        Me.Btn3.Location = New System.Drawing.Point(370, 191)
+        Me.Btn3.Name = "Btn3"
+        Me.Btn3.Size = New System.Drawing.Size(75, 23)
+        Me.Btn3.TabIndex = 5
+        Me.Btn3.Text = "Button3"
+        Me.Btn3.UseVisualStyleBackColor = True
+        '
+        'Btn4
+        '
+        Me.Btn4.Location = New System.Drawing.Point(510, 191)
+        Me.Btn4.Name = "Btn4"
+        Me.Btn4.Size = New System.Drawing.Size(75, 23)
+        Me.Btn4.TabIndex = 6
+        Me.Btn4.Text = "Button4"
+        Me.Btn4.UseVisualStyleBackColor = True
+        '
+        'Btn5
+        '
+        Me.Btn5.Location = New System.Drawing.Point(647, 191)
+        Me.Btn5.Name = "Btn5"
+        Me.Btn5.Size = New System.Drawing.Size(75, 23)
+        Me.Btn5.TabIndex = 7
+        Me.Btn5.Text = "Button5"
+        Me.Btn5.UseVisualStyleBackColor = True
+        '
+        'BtnExit
+        '
+        Me.BtnExit.Location = New System.Drawing.Point(353, 367)
+        Me.BtnExit.Name = "BtnExit"
+        Me.BtnExit.Size = New System.Drawing.Size(75, 23)
+        Me.BtnExit.TabIndex = 8
+        Me.BtnExit.Text = "Exit"
+        Me.BtnExit.UseVisualStyleBackColor = True
+        '
+        'LblFrench
+        '
+        Me.LblFrench.AutoSize = True
+        Me.LblFrench.BackColor = System.Drawing.Color.Black
+        Me.LblFrench.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblFrench.ForeColor = System.Drawing.Color.Fuchsia
+        Me.LblFrench.Location = New System.Drawing.Point(367, 274)
+        Me.LblFrench.Name = "LblFrench"
+        Me.LblFrench.Size = New System.Drawing.Size(57, 18)
+        Me.LblFrench.TabIndex = 9
+        Me.LblFrench.Text = "Label4"
+        Me.LblFrench.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LblFrench)
+        Me.Controls.Add(Me.BtnExit)
+        Me.Controls.Add(Me.Btn5)
+        Me.Controls.Add(Me.Btn4)
+        Me.Controls.Add(Me.Btn3)
+        Me.Controls.Add(Me.Btn2)
+        Me.Controls.Add(Me.Btn1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "French Numbers"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents BtnExit As Button
+    Friend WithEvents LblFrench As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Public Class Form1
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        LblFrench.Visible = True
+        LblFrench.Text = "un"
+
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles LblFrench.Click
+
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        LblFrench.Visible = True
+        LblFrench.Text = "deux"
+    End Sub
+
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        LblFrench.Visible = True
+        LblFrench.Text = "trois"
+    End Sub
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        LblFrench.Visible = True
+        LblFrench.Text = "quatre"
+    End Sub
+
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles Btn5.Click
+        LblFrench.Visible = True
+        LblFrench.Text = "cing"
+    End Sub
+
+    Private Sub BtnExit_Click(sender As Object, e As EventArgs) Handles BtnExit.Click
+        Close()
+
+    End Sub
+End Class
